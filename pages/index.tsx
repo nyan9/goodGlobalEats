48f61221fe240a18c327a9d843d675b054ca6de5
nyan9 <rynnaing@gmail.com>
2021-10-09 23:37:24 -0400

    initial layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 // import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
-// import Layout from "src/components/layout";
+import Layout from "src/components/layout";
 // import Map from "src/components/map";
 // import SpotList from "src/components/spotList";
 // import { useLastData } from "src/utils/useLastData";
@@ -9,5 +9,5 @@
 // import { SpotsQuery, SpotsQueryVariables } from "src/generated/SpotsQuery";
 
 export default function Spot() {
-  return <div>Spot</div>;
+  return <Layout main={<div>Spot</div>} />;
 }
