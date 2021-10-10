27197c720b10be7a656371c22663d179320efa7f
nyan9 <rynnaing@gmail.com>
2021-10-10 18:16:17 -0400

    add login/signup function and auth routes

--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,38 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FunctionComponent, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+const firebaseAuthConfig = {
+  signInFlow: "popup",
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  const [renderAuth, setRenderAuth] = useState(false);
+
+  // styledFirebaseAuth sometimes has a problem where it doesn't like to be rendered on the server
+  // lifecycle trick to only render styledFirebaseAuth after the first render (after server-side render)
+  useEffect(() => {
+    setRenderAuth(true);
+  }, []);
+
+  return (
+    <div className="mt-16">
+      {renderAuth && (
+        <StyledFirebaseAuth
+          uiConfig={firebaseAuthConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
