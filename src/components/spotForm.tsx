e8b812477e02c54cc28a5b8d589f718bbcc4c761
nyan9 <rynnaing@gmail.com>
2021-10-12 01:25:43 -0400

    add text inputs to spotForm

--- src/components/spotForm.tsx
+++ src/components/spotForm.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
 // import {
@@ -19,7 +19,9 @@ interface IFormData {
   address: string;
   latitude: number;
   longitude: number;
-  bedrooms: string;
+  appetizer: string;
+  entree: string;
+  drink: string;
   image: FileList;
 }
 
@@ -27,9 +29,11 @@ interface IProps {}
 
 export default function SpotForm({}: IProps) {
   const [submitting, setSubmitting] = useState(false);
+  const [previewImage, setPreviewImage] = useState<string>();
   const { register, handleSubmit, setValue, errors, watch } = useForm<
     IFormData
   >({ defaultValues: {} });
+  const address = watch("address");
 
   useEffect(() => {
     register(
@@ -40,7 +44,9 @@ export default function SpotForm({}: IProps) {
     register({ name: "longitude" }, { required: true, min: -180, max: 180 });
   }, [register]);
 
-  const handleCreate = async (data: IFormData) => {};
+  const handleCreate = async (data: IFormData) => {
+    console.log(data);
+  };
 
   const onSubmit = (data: IFormData) => {
     setSubmitting(false);
@@ -66,6 +72,105 @@ export default function SpotForm({}: IProps) {
 
         {errors.address && <p>{errors.address.message}</p>}
       </div>
+
+      {address && (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              Click to add image
+            </label>
+            <input
+              id="image"
+              name="image"
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) return true;
+                  return "Please upload one file";
+                },
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+            />
+
+            {previewImage && (
+              <img
+                src={previewImage}
+                className="mt-4 object-cover"
+                style={{ width: "576px", height: `${(16 / 9) * 576}px` }}
+              />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="recommended_app" className="block">
+              Must Try Appetizer:
+            </label>
+            <input
+              id="recommended_app"
+              name="appetizer"
+              type="text"
+              className="p-2"
+              ref={register()}
+            />
+            {errors.appetizer && <p>{errors.appetizer.message}</p>}
+
+            <label htmlFor="recommended_entree" className="block">
+              Must Try Entree:
+            </label>
+            <input
+              id="recommended_entree"
+              name="entree"
+              type="text"
+              className="p-2"
+              ref={register()}
+            />
+            {errors.entree && <p>{errors.entree.message}</p>}
+
+            <label htmlFor="recommended_drinks" className="block">
+              Must Try Drinks:
+            </label>
+            <input
+              id="recommended_drink"
+              name="drink"
+              type="text"
+              className="p-2"
+              ref={register()}
+            />
+            {errors.drink && <p>{errors.drink.message}</p>}
+          </div>
+
+          <div className="mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded mr-6"
+              type="submit"
+              disabled={submitting}
+            >
+              Put everybody on!
+            </button>
+
+            <div className="bg-red-500 hover:bg-red-700 font-bold py-2 px-4 rounded inline-block">
+              <Link href="/">
+                <a>Cancel</a>
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
     </form>
   );
 }
