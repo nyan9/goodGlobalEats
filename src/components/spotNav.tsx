253f09c997322cd25bf4d925ca125538d077efc8
nyan9 <rynnaing@gmail.com>
2021-10-17 01:22:05 -0400

    add delete mutation to spotNav

--- src/components/spotNav.tsx
+++ src/components/spotNav.tsx
@@ -2,7 +2,13 @@ import { useMutation, gql } from "@apollo/client";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useAuth } from "src/auth/useAuth";
-// import { DeleteSpot, DeleteSpotVariables } from "src/generated/DeleteSpot";
+import { DeleteSpot, DeleteSpotVariables } from "src/generated/DeleteSpot";
+
+const DELETE_MUTATION = gql`
+  mutation DeleteSpot($id: String!) {
+    deleteSpot(id: $id)
+  }
+`;
 
 interface IProps {
   spot: {
@@ -13,7 +19,12 @@ interface IProps {
 
 export default function SpotNav({ spot }: IProps) {
   const { user } = useAuth();
+  const router = useRouter();
   const canEdit = !!user && user.uid === spot.userId;
+  const [deleteSpot, { loading }] = useMutation<
+    DeleteSpot,
+    DeleteSpotVariables
+  >(DELETE_MUTATION);
 
   return (
     <>
@@ -23,9 +34,25 @@ export default function SpotNav({ spot }: IProps) {
       {canEdit && (
         <>
           {" | "}
+
           <Link href={`/spots/${spot.id}/edit`}>
             <a>Edit</a>
           </Link>
+
+          {" | "}
+
+          <button
+            disabled={loading}
+            type="button"
+            onClick={async () => {
+              if (confirm("Are you sure you want to delete this spot?")) {
+                await deleteSpot({ variables: { id: spot.id } });
+                router.push("/");
+              }
+            }}
+          >
+            DELETE
+          </button>
         </>
       )}
     </>
