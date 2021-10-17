b038d9d485099a749ac12f89fce13a30b3596fed
nyan9 <rynnaing@gmail.com>
2021-10-17 01:23:42 -0400

    remove dummy resolver from schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,18 +1,10 @@
-import { buildSchemaSync, Query, Resolver } from "type-graphql";
+import { buildSchemaSync } from "type-graphql";
 import { ImageResolver } from "./image";
 import { SpotResolver } from "./spot";
 import { authChecker } from "./auth";
 
-@Resolver()
-class DummyResolver {
-  @Query((_returns) => String)
-  hello() {
-    return "Nice to see you";
-  }
-}
-
 export const schema = buildSchemaSync({
-  resolvers: [DummyResolver, ImageResolver, SpotResolver],
+  resolvers: [ImageResolver, SpotResolver],
   emitSchemaFile: process.env.NODE_ENV === "development",
   authChecker,
 });
